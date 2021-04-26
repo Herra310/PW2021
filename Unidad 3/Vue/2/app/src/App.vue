c2361f768ba78b3da9c6bf27027279f63528516d
Jorge Herrera <jorgehr99jhr@gmail.com>
2021-04-26 11:25:52 -0600

    Primer vistazo al CLI

---
+++ Unidad 3/Vue/2/app/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <HelloWorld :msg="titulo" fecha="22-abril-2021"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return{
+        titulo: "Mi titulo de vue"
+      }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
